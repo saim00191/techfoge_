642d244914c2fe262ab4be8a4d641330146d403b
Saim Raza <143351556+saim00191@users.noreply.github.com>
2025-07-08 01:35:53 -0700

    Update page.tsx

--- app/services/web-development/page.tsx
+++ app/services/web-development/page.tsx
@@ -2,7 +2,6 @@ import WebDevelopmentServices from '@/components/Services/web-development'
 import React from 'react'
 
 import type { Metadata } from "next"
-
 export const metadata: Metadata = {
   title: "Web Development Agency",
   description:
