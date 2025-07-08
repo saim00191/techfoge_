5c7647839fa93fa2123deb1c75936cd1b77f667b
Saim Raza <143351556+saim00191@users.noreply.github.com>
2025-07-08 13:27:43 -0700

    Update page.tsx

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -5,7 +5,24 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Blog",
-  description: "Explore the latest insights, tutorials, and updates on software, AI, mobile apps, and digital innovation from TechFoge.",  
+  description: "Explore the latest insights, tutorials, and updates on software, AI, mobile apps, and digital innovation from TechFoge.",
+   openGraph: {
+    title: "TechFoge Blog",
+    description:
+      "Stay updated with TechFoge’s blog – featuring expert tips, industry news, and guides on web development, AI, design, and more.",
+    url: "https://techfoge.com/blogs",
+    siteName: "TechFoge",
+    images: [
+      {
+        url: "https://techfoge.com/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "TechFoge Blog - News, Insights, Tutorials",
+      },
+    ],
+    locale: "en_US",
+    type: "blog",
+  },
   alternates: {
     canonical: "https://techfoge.com/blogs",
   },
