67e8867ff563224f284297b7c44faa63f2b715fb
Saim Raza <143351556+saim00191@users.noreply.github.com>
2025-07-08 05:55:49 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,6 +111,19 @@ export default function RootLayout({
   return (
     <html lang="en">
        <head>
+          <Script
+    id="gtm-script"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TNSBJ6MV');
+      `,
+    }}
+  />
         <Script
           id="structured-data"
           type="application/ld+json"
