02338fde98922327fb6909f126f7468f90cf4562
Saim Raza <143351556+saim00191@users.noreply.github.com>
2025-07-09 03:22:18 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -152,6 +152,8 @@ export default function RootLayout({
             }),
           }}
         />
+         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3033083111673877"
+     crossorigin="anonymous"></script>
       </head>
       <body className={` ${poppins.className}`}>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TNSBJ6MV"
